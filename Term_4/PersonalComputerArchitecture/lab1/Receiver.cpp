9afa75f4332dccbdf3b9d9418e5a2409aa59d932
ni0x0 <nik.x.i0ta@gmail.com>
2024-11-11 05:51:47 +0300

    PCA added

---
+++ Term_4/PersonalComputerArchitecture/lab1/Receiver.cpp
@@ -0,0 +1,67 @@
+﻿#include <windows.h>
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+void ReadCOM();            // ф-я чтения из com'а
+
+HANDLE hSerPortHandler;    // "описатель com порта" 
+
+int main() {
+
+    LPCTSTR serPortName = L"COM2";
+
+    // Открываем порт для чтения
+    // имя, режим доступа, режим совместного доступа (д.б. запрещен), атрибут защиты файла (для com нет)б
+    // управление режимом автосоздания файлом (для com всегда один и тот же), атрибут файла, описатель файла "шаблона" (для com не используется)
+    hSerPortHandler = ::CreateFile(serPortName, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+
+    // Проверка работоспособности
+    if (hSerPortHandler == INVALID_HANDLE_VALUE) {
+        if (GetLastError() == ERROR_FILE_NOT_FOUND) {
+            cout << "SERIAL PORT DOESN'T EXIST\n";
+        }
+        cout << "OTHER ERROR\n";
+        return 0;
+    }
+    else cout << "COM2 (RECIEVER) IS FOUND\n\n";
+
+
+    // Настройка параметров соединения  
+    DCB dcbSerialParams = { 0 };
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);        // длина структуры
+    if (!GetCommState(hSerPortHandler, &dcbSerialParams)) {
+        cout << "GET STATE ERROR\n";
+        return 0;
+    }
+    dcbSerialParams.BaudRate = CBR_9600;                  // скорость в бит/сек
+    dcbSerialParams.ByteSize = 8;                         // количество бит в символе 
+    dcbSerialParams.StopBits = ONESTOPBIT;                // длина стопового бита 
+    dcbSerialParams.Parity = NOPARITY;                    // режим паритета
+    if (!SetCommState(hSerPortHandler, &dcbSerialParams)) {
+        cout << "SET STATE ERROR\n";
+        return 0;
+    }
+
+    // чтение из порта
+    ReadCOM();
+
+    // закрыть порт
+    CloseHandle(hSerPortHandler);
+    return 0;
+}
+
+// Чтение
+void ReadCOM()
+{
+    DWORD iSize;
+    char sReceivedChar;
+    while (true) {
+        BOOL iRet = ReadFile(hSerPortHandler, &sReceivedChar, 1, &iSize, 0);  // получаем 1 байт
+        if (iSize > 0 && iRet) {                                              // если что-то принято, выводим
+            if (sReceivedChar == '$')
+                break;
+            cout << sReceivedChar;
+        }
+    }
+}
---
+++ Term_4/PersonalComputerArchitecture/lab1/Sender.cpp
@@ -0,0 +1,62 @@
+﻿#include <windows.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    HANDLE hSerPortHandler;                 // "описатель com порта" 
+    LPCTSTR sPortName = L"COM1";
+    //LPCTSTR sPortName = L"COM2";
+
+    // Открываем ком порт для записи
+    // имя, режим доступа, режим совместного доступа (д.б. запрещен), атрибут защиты файла (для com нет)б
+    // управление режимом автосоздания файлом (для com всегда один и тот же), атрибут файла, описатель файла "шаблона" (для com не используется)
+    hSerPortHandler = ::CreateFile(sPortName, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+    
+
+    // Проверка работоспособности
+    if (hSerPortHandler == INVALID_HANDLE_VALUE) {
+        if (GetLastError() == ERROR_FILE_NOT_FOUND) {
+            cout << "SERIAL PORT DOESN'T EXIST\n";
+        }
+        cout << "OTHER ERROR\n";
+        return 0;
+    }
+    else cout << "COM (SENDER) IS FOUND\n\n";
+
+
+    // Настройка параметров соединения
+    DCB dcbSerialParams = { 0 };
+    dcbSerialParams.DCBlength = sizeof(dcbSerialParams);  // длина структуры
+    if (!GetCommState(hSerPortHandler, &dcbSerialParams)) {
+        cout << "GET STATE ERROR\n";
+        return 0;
+    }
+    dcbSerialParams.BaudRate = CBR_9600;                  // скорость в бит/сек
+    dcbSerialParams.ByteSize = 8;                         // количество бит в символе 
+    dcbSerialParams.StopBits = ONESTOPBIT;                // длина стопового бита 
+    dcbSerialParams.Parity = NOPARITY;                    // режим паритета
+    if (!SetCommState(hSerPortHandler, &dcbSerialParams)) {
+        cout << "SET STATE ERROR\n";
+        return 0;
+    }
+
+    // отсылка данных
+    while (1) {
+        char data_to_send[10] = {'\0'};              // строка для передачи
+        cin >> data_to_send;
+
+        DWORD dwSize = sizeof(data_to_send);         // размер этой строки
+        DWORD dwBytesWritten;                        // количество байт, что будет передано
+        // отправка строки
+        BOOL iRet = WriteFile(hSerPortHandler, data_to_send, dwSize, &dwBytesWritten, NULL);
+        //cout << dwSize << " Bytes in string. " << dwBytesWritten << " Bytes sended. " << endl;
+
+        if (data_to_send[0] == '$')
+            break;
+    }
+
+    // закрыть порт
+    CloseHandle(hSerPortHandler);
+    return 0;
+}
